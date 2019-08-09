43bba260ebf16380add1c25dcabe65ff97dabc00
gql <gql@111.com.cn>
2019-08-09 10:18:46 +0800

    Update auto_jk_jobs.sql

--- mysql_db/auto_jk_jobs.sql
+++ mysql_db/auto_jk_jobs.sql
@@ -10,13 +10,11 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Table structure for `auto_jk_jobs`
 -- ----------------------------
 DROP TABLE IF EXISTS `auto_jk_jobs`;
+
 CREATE TABLE `auto_jk_jobs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `jk_name` varchar(100) NOT NULL,
   `jk_desc` varchar(500) NOT NULL,
+  `jk_server` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of auto_jk_jobs
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=utf8;
